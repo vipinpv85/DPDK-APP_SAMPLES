ad0cc89d06c0785ce85ba48e1b1f87bacc2eb017
Vipin Varghese <vipinpv85@gmail.com>
2021-02-15 12:41:55 +0530

    Update rteflow_i40e_tap.c

--- rteflow_i40e_tap.c
+++ rteflow_i40e_tap.c
@@ -46,3 +46,57 @@
         else
                 printf("rte_flow err %s\n", error.message);
 }
+
+
+{
+        struct rte_flow_action actions[10];
+struct rte_flow_item_eth eth;
+struct rte_flow_item_eth eth_mask;
+struct rte_flow_item_vlan vlan;
+struct rte_flow_item_vlan vlan_mask;
+struct rte_flow_item_ipv4 ipv4;
+struct rte_flow_item_ipv4 ipv4_mask;
+struct rte_flow *flow;
+struct rte_flow_action_mark mark = { .id = 0xdeadbeef };
+struct rte_flow_action_queue queue = { .index = 0x3 };
+
+memset(&pattern, 0, sizeof(pattern));
+memset(&actions, 0, sizeof(actions));
+memset(&attr, 0, sizeof(attr));
+attr.group = 0;
+attr.priority = 0;
+attr.ingress = 1;
+attr.egress = 0;
+
+memset(&eth_mask, 0, sizeof(struct rte_flow_item_eth));
+pattern[0].type = RTE_FLOW_ITEM_TYPE_ETH;
+pattern[0].spec = &eth;
+pattern[0].last = NULL;
+pattern[0].mask = NULL;
+
+memset(&vlan_mask, 0, sizeof(struct rte_flow_item_vlan));
+pattern[1].type = RTE_FLOW_ITEM_TYPE_VLAN;
+pattern[1].spec = &vlan;
+pattern[1].last = NULL;
+pattern[1].mask = NULL;
+
+
+/* set the dst ipv4 packet to the required value */
+pattern[1].type = RTE_FLOW_ITEM_TYPE_IPV4;
+pattern[1].spec = NULL;
+pattern[1].last = NULL;
+pattern[1].mask = NULL;
+
+pattern[2].type = RTE_FLOW_ITEM_TYPE_UDP;
+pattern[2].spec = NULL;
+pattern[2].last = NULL;
+pattern[2].mask = NULL;
+
+/* end the pattern array */
+pattern[3].type = RTE_FLOW_ITEM_TYPE_END;
+
+/* create the drop action */
+actions[0].type = RTE_FLOW_ACTION_TYPE_MARK;
+actions[0].conf = &mark;
+actions[1].type = RTE_FLOW_ACTION_TYPE_END;
+}
